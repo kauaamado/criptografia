0dcda62c73806d8336939d1f52ccb149cce469e7
Kauã Amado <kauaamado5@gmail.com>
2026-05-19 10:52:18 -0300

    feat: lógica do quadrado de polibio

---
+++ cifras/adfgvx.py
@@ -0,0 +1,71 @@
+# Algoritmo de encriptação ADFGVX
+
+from utilitarios import Utils
+
+alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+numeros = '1234567890'
+
+quadrado_de_polibio = [[], [], [], [], [], []] # linha, coluna
+
+menu = {
+    1: 'Encriptar',
+    2: 'Desencriptar',
+    3: 'Sair'}
+
+def encriptar(chave, texto):
+    chave = Utils(chave)
+    texto = texto.lower()
+
+    chave_limpa = chave.limpar()
+
+    print(chave_limpa)
+    print(texto)
+
+    conteudo_quadrado = "" + chave_limpa + alfabeto
+    conteudo_quadrado = Utils(conteudo_quadrado).limpar()
+    print(conteudo_quadrado)
+    conteudo_quadrado = list(conteudo_quadrado)
+    for letra in alfabeto[:10]:
+        posicao_no_quadrado = conteudo_quadrado.index(letra)
+        conteudo_quadrado.insert(posicao_no_quadrado + 1, numeros[alfabeto.index(letra)])
+    print("Conteudo por extenso: ")
+    
+    print(conteudo_quadrado)
+    print("Quantidade: " + str(len(conteudo_quadrado)))
+
+    print("Quadrado de polibio")
+
+    for i in range(6):
+        for j in range(6):
+            quadrado_de_polibio[i].append(conteudo_quadrado[i * 6 + j])
+
+    for i in quadrado_de_polibio:
+        print(i)
+
+while True:
+    print('===========================================')
+    print('Bem vindo ao algoritmo de cifragem ADFGVX')
+    print('===========================================')
+    for i in menu:
+        print(i , '-', menu[i])
+    print('===========================================')
+
+    try:
+        opcao = input('Escolha uma opção: ')
+
+        print(opcao)
+
+        if opcao == '1':
+            print('Encriptando...')
+            
+            chave = input('Insira a chave: ')
+            texto = input('Insira o texto: ')
+            
+            encriptar(chave, texto)
+        elif opcao == '2':
+            pass
+        elif opcao == '3':
+            print('Saindo...')
+            break
+    except ValueError:
+        print('Opção inválida')
